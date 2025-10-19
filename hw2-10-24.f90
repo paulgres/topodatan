50d3d250736d7d42cf5cb3d52d668440c6aec82b
Paul Gres <paul.gres@gmail.com>
2025-10-20 00:58:02 +0200

    matrices

--- hw2-10-24.f90
+++ hw2-10-24.f90
@@ -7,28 +7,34 @@ program hw2
     integer, parameter:: n=4,m=2
     logical::d1(n,n),d2(n,n),fa(n,n,n),ed(n,n)
     integer::i,j,k,tt(n),k1,k2,f,l,k3
-    real:: eps,d,ts1(m*2,n*(n-1)/2),dvr(n*(n-1)/2),dch(n*(n-1)) 
+    real:: eps,d,r,ts1(m*2,n*(n-1)/2),dvr1(n,n),dch1(n,n) , dvr2(n,n,n), dch2(n,n,n)
     real, dimension(2,4) ::pts
     pts = reshape((/ -1.,0.,1.,0.,0.,-2.,0.,2./), shape=(/m,n/))
 
 
-    eps = 4!2.25
+    eps = 4
     write (*, '(2f6.2)') pts
 k1=0
 k2=0
 k3=0
-dvr=0.0
-dch=0.0 
+dvr1=-1.0
+dch1=-1.0 
+dvr2=-1.0
+dch2=-1.0 
 !dmat=10*epsilon
 do i = 1,n-1
   k=0
   f=0
   do j = i+1,n
-    d = anint(norm2(pts(:,i)-pts(:,j))*1000.0)/1000.0
-    k2=k2+1
-    k3=k3+1
-    dvr(k2) = d
-    dch(k3) = d/2.0
+    if (dvr1(j,i).le.0.0) then
+      d = anint(norm2(pts(:,i)-pts(:,j))*1000.0)/1000.0
+      k2=k2+1
+      k3=k3+1
+      dvr1(j,i) = d
+      dch1(j,i) = d/2.0
+    else
+      d=dvr1(j,i)
+    end if
     if (d.le.eps) then
       k=k+1
       tt(k) = j
@@ -42,9 +48,15 @@ do i = 1,n-1
   end do
   do j = i+1, n-1
       do l = j+1,n
-        d=anint(trad(pts(:,i),pts(:,j),pts(:,l))*1000.0)/1000.0
+        d = anint(norm2(pts(:,l)-pts(:,j))*1000.0)/1000.0
+        dvr1(l,j)=d
+        dch1(l,j)=d/2.0
+        d = max(max(dvr1(i,j),dvr1(i,l)),d)
+        dvr2(i,j,l)=d
+        r=anint(trad(pts(:,i),pts(:,j),pts(:,l))*1000.0)/1000.0
         k3=k3+1
-        dch(k3) = d
+        
+        dch2(i,j,l) = r
       end do
     end do
   
@@ -54,10 +66,10 @@ print *, "k2 =  ", k2
 print *, "k3 =  ", k3
 print *
 print *, "Viettoris-Rips"
-write (*, '(f6.2)') dvr
+write (*, '(4f6.2)') dvr1
 print *
 print *, "Cech"
-write (*, '(f6.2)') dch
+write (*, '(4f6.2)') dch1
 open(newunit=f,file='edges.txt',status='replace',action='write',iostat=f)
 write (f,'(4(1x,ES19.12))',err=501) ts1(:,1:k1)
 501 close(f)
